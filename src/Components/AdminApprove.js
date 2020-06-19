a38b8c6f0ce6b178f94e9a175e4cec33f3069fb2
vitor-slopes <vitor_slopes@hotmail.com>
2020-06-19 12:40:23 -0300

    Components added

---
+++ src/Components/AdminApprove.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react'
+
+export default function AdminApprove() {
+  return (
+    <div>AdminApprove Component</div>
+  )
+}
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -7,6 +7,8 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Hidden from '@material-ui/core/Hidden';
+import { useSelector } from 'react-redux'
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -22,32 +24,37 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
+export default function Header() {
+  const classes = useStyles();
+  // const [freeUser, setFreeUser] = React.useState("não clicado")
+
+  const freeUser = useSelector((state) => state.header.freeUser)
 
 
-  export default function ButtonAppBar() {
-    const classes = useStyles();
-    // const [mobileOpen, setMobileOpen] = React.useState(false);
+  const handleDrawerToggle = () => {
+    // setMobileOpen(!mobileOpen);
+  };
 
-    const handleDrawerToggle = () => {
-      // setMobileOpen(!mobileOpen);
-    };
 
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <Hidden smUp>
-              <IconButton onClick={handleDrawerToggle} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                <MenuIcon />
-              </IconButton>
-            </Hidden>
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Hidden smUp>
+            <IconButton onClick={handleDrawerToggle} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
 
-            <Typography variant="h6" className={classes.title}>
-              News
+          <Typography variant="h6" className={classes.title}>
+            SPOTENU
           </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
+          <Button color="inherit">{freeUser}</Button>
+          {/* <Button onClick={props.vipUser} color="inherit">VipUser</Button>
+            <Button onClick={props.admin} color="inherit">Admin</Button>
+            <Button onClick={props.musician} color="inherit">Musician</Button> */}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
