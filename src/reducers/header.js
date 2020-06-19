6eaf7e2d89ad11dd4a2ddbe9da80b6e4d11e1e7e
vitor-slopes <vitor_slopes@hotmail.com>
2020-06-19 12:48:22 -0300

    setting initial reducers and actions

---
+++ src/reducers/header.js
@@ -0,0 +1,16 @@
+const initialState = {
+  teste: 'aaakeehh',
+  freeUser: "freeUser",
+  vipUser: "vipUser",
+  admin: "admin",
+  band: "band",
+}
+
+export function header(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default header
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
+import {header} from './header'
 
 export const generateReducers = history =>
   combineReducers({
-    router: connectRouter(history)
+    router: connectRouter(history),
+    header
   });
